10a02dff9b8229a2c1d469bbbbd39699a2467991
lauralcodes <charish.weerasuriya@gmail.com>
2026-05-28 23:55:48 +0200

    Initial commit

---
+++ src/test/java/se/jensen/charitha/cloudstore/CloudStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jensen.charitha.cloudstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
